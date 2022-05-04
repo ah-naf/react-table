0ccd0f49797a493c0eb95b6325d61080ff197b42
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2022-05-04 23:05:26 +0600

    Update Homepage.js

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import DataTable from "../DataTable/DataTable";
 import DataTableList from "../DataTableList/DataTableList";
 
-
 const Homepage = () => {
   const [tableData, setTableData] = useState([]);
 
@@ -13,16 +12,18 @@ const Homepage = () => {
       .then((data) => setTableData(data));
   }, []);
   return (
-    <div className="flex ">
-      <div className="hidden sm:inline-flex">
-        <div className="p-6 px-4 lg:px-7 space-y-1">
+    <div className="flex  flex-col-reverse sm:flex-row">
+      <div className="flex sm:inline-flex">
+        <div className="flex justify-between items-center w-full bg-white p-2 sm:p-6 px-4 lg:px-7 space-y-1 sm:bg-transparent sm:flex-col sm:justify-start relative before:absolute before:w-full before:h-1 before:bg-black before:opacity-10 before:top-0 before:left-0 sm:before:opacity-0">
           <div className="flex items-center space-x-2 p-3">
             <img src="/1.svg" alt="" />
             <h3 className="text-gray-700 hidden md:flex">Dashboard</h3>
           </div>
-          <div className="flex items-center space-x-2 bg-white p-3 rounded-md shadow-md">
+          <div className="flex items-center space-x-2 bg-white p-3 rounded-md sm:shadow-md">
             <img src="/2.svg" alt="" />
-            <h3 className="text-green-500 hidden font-medium md:flex">Inventory</h3>
+            <h3 className="text-green-500 hidden font-medium md:flex">
+              Inventory
+            </h3>
           </div>
           <div className="flex items-center space-x-2 p-3">
             <img src="/3.svg" alt="" />
@@ -34,8 +35,10 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-      <div className="flex-grow border-2 border-b-0">
-        <h2 className="text-2xl p-6 pb-4 font-medium text-gray-800">Inventory</h2>
+      <div className="flex-grow border-2 border-b-0 mb-6">
+        <h2 className="text-2xl p-6 pb-4 font-medium text-gray-800">
+          Inventory
+        </h2>
         <div className="p-5 border-2 border-b-0">
           <DataTableList />
         </div>
